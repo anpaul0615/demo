3f40b370b9cc8e50a6643001d92d8b6b18751fc2
PAUL AN <anpaul0615@gmail.com>
2018-08-24 15:01:23 +0900

    Add react-contextapi-demo

---
+++ react/react-contextapi/README.md
@@ -0,0 +1,3 @@
+# react-contextapi-demo
+
+> https://itnext.io/understanding-the-react-context-api-through-building-a-shared-snackbar-for-in-app-notifications-6c199446b80c  
---
+++ react/react-contextapi/src/components/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ButtonA from './ButtonA';
+import ButtonB from './ButtonB';
+import { SharedSnackbarProvider } from './SharedSnackbar.context';
+
+const styles = {
+  app: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={styles.app}>
+        <SharedSnackbarProvider>
+          <ButtonA />
+          <ButtonB />
+        </SharedSnackbarProvider>
+      </div>
+    );
+  }
+}
+
+export default App;
+
