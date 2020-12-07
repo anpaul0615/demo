5830bbd1d7adafeda7716c9d7beed100e887901c
Paul An <anpaul0615@gmail.com>
2020-12-07 21:56:56 +0900

    Add scss-with-cli

---
+++ scss/scss-with-cli/src/App.js
@@ -0,0 +1,6 @@
+const appDiv = document.createElement('div');
+appDiv.id = 'app';
+appDiv.innerHTML += '<p>greeting!!</p>';
+appDiv.innerHTML += '<p class="foo">hello foo!!</p>';
+appDiv.innerHTML += '<p class="bar">hello bar!!</p>';
+document.body.appendChild(appDiv);
